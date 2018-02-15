f029fd7163865cd33e9db6325350908655c10a98
Travis Cotney <tcotney96@gmail.com>
2018-02-15 17:28:04 -0600

    Working on the punch collection method.

---
+++ src/user/datagram/protocol/UserDatagramProtocol.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package user.datagram.protocol;
+import java.awt.event.*;
+import java.nio.*;
+import javax.swing.*;
+import javax.swing.Timer;
+import java.net.*;
+
+/**
+ *
+ * @author Travis
+ */
+public class UserDatagramProtocol {
+    ByteBuffer buffer = new ByteBuffer(100);
+    DatagramPacket packet = new DatagramPacket(buffer, 100);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Actionlistener listener new ActionListener
+        count++;
+        // TODO code application logic here
+    }
+    
+}
