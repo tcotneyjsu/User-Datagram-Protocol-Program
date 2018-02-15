d065ceedcc980959c301804de9aa85bf12dc28c7
Travis Cotney <tcotney96@gmail.com>
2018-02-15 17:56:35 -0600

    Working on the punch collection method.

--- src/user/datagram/protocol/UserDatagramProtocol.java
+++ src/user/datagram/protocol/UserDatagramProtocol.java
@@ -9,22 +9,43 @@ import java.nio.*;
 import javax.swing.*;
 import javax.swing.Timer;
 import java.net.*;
+import java.net.DatagramSocket;//Had to import both because of constructor errors
+import java.net.DatagramPacket;//Had to import both because of constructor errors
 
 /**
  *
  * @author Travis
  */
-public class UserDatagramProtocol {
-    ByteBuffer buffer = new ByteBuffer(100);
-    DatagramPacket packet = new DatagramPacket(buffer, 100);
-
+public class UserDatagramProtocol extends Thread{
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buffer = new byte[512];
+    
+    public UserDatagramProtocol(){
+        int port = 2000;
+        try{
+            socket = new DatagramSocket(port);
+        }
+        catch(Exception e){}
+    }
+    public void running(){
+        running = true;
+        while(running){
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            try{
+                socket.receive(packet);
+            }
+            catch(Exception e){};
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(buffer, buffer.length, address, port);
+        }
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        Actionlistener listener new ActionListener
-        count++;
+
         // TODO code application logic here
     }
     
