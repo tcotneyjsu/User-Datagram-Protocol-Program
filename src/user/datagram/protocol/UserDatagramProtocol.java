6b97d4299f65eb59939dbe1fb8fd156ebe5d1d67
Travis Cotney <tcotney96@gmail.com>
2018-02-25 17:39:46 -0600

    Its Final

--- src/user/datagram/protocol/UserDatagramProtocol.java
+++ src/user/datagram/protocol/UserDatagramProtocol.java
@@ -19,13 +19,16 @@ import java.net.DatagramPacket;//Had to import both because of constructor error
  */
 public class UserDatagramProtocol extends Thread{
     private DatagramSocket socket;
-    private boolean running;
+    private boolean received = false;
     private byte[] buffer = new byte[256];
     private Random random;
     long startTime;
     long stopTime;
+    long waitTime;
     long totalDuration;
-    int clientSent;
+    long[] pings;
+    int clientSent = 1;
+    
     public UserDatagramProtocol(){
         random = new Random();
         Thread thread_2 = new UDClient();
@@ -35,21 +38,25 @@ public class UserDatagramProtocol extends Thread{
     }
     
     class UDServer extends Thread{
+        @Override
         public void run(){
+
             try{
+                
+                    
                 // Create a datagram socket, bound to the specific port 2000
                 DatagramSocket socket = new DatagramSocket(2000);
 
                 System.out.println ("Bound to local port " + socket.getLocalPort());
-
-                    // Create a datagram packet, containing a maximum buffer of 256 bytes
+                while(true){ 
+                    // Create a datagram packet, containing a maximum buffer of 256 byte 
                 DatagramPacket packet = new DatagramPacket( new byte[256], 256 );
 
                 // Receive a packet - remember by default this is a blocking operation
+                
                 socket.receive(packet);
-                stopTime = System.currentTimeMillis();
-                long totalDuration = stopTime - startTime;
-                if(random.nextInt(2) == 1){
+                if(random.nextInt(3) != 1){
+                    received = true;
                     System.out.println ("Packet received at " + new Date( ));
                     // Display packet information
                     InetAddress remote_addr = packet.getAddress();
@@ -67,25 +74,38 @@ public class UserDatagramProtocol extends Thread{
                             else System.out.print ( (char) data);
                             
                     }
-                    socket.close( );
-                    
+                    try{
+                        int whatislove = random.nextInt(201);
+                        System.out.println(""+whatislove);
+                        Thread.sleep(whatislove);
+                    }
+                    catch(Exception e){}
+                    socket.send(packet);
                 }
                 else{
-                    socket.close();
-                    System.out.println("Did not receive packet, now Hangry c(-_-c), "
+                    System.out.println("Did not receive packet " + clientSent
+                            + ", now Hangry c(-_-c), "
                             + "continuing to next iterration.");
+                    
+                    clientSent++;
+                    
                 }
+                
             }
+            }   
             catch (IOException e) 	{
                     System.out.println ("Error - " + e);
             }
+            
         }
-    }    
-    //Im doing this for you trent
+    }
     class UDClient extends Thread{
         //use localhost to experiment on a standalone computer
+        @Override
         public void run(){
         String hostname="localhost";    String message = "HELLO USING UDP!";
+        while(clientSent < 11){
+            startTime = System.currentTimeMillis();
             try {
 		// Create a datagram socket, look for the first available port
 		DatagramSocket socket = new DatagramSocket();
@@ -111,15 +131,27 @@ public class UserDatagramProtocol extends Thread{
                         Random random = new Random();
                        //if(random.nextInt(2) == 1){
                             socket.send(packet);
-                            startTime = System.currentTimeMillis();
+                            //startTime = System.currentTimeMillis();
                        //}
-                        socket.close();
+                       //socket.close();
 		System.out.println ("Packet sent at! " + new Date());
 
 		// Display packet information
 		System.out.println ("Sent by  : " + remote_addr.getHostAddress() );
         		System.out.println ("Send from: " + packet.getPort());
-
+                waitTime = System.currentTimeMillis();
+                while((System.currentTimeMillis()-waitTime) < 300 && !received){
+                }
+                if(received){
+                    socket.receive(packet);
+                    stopTime = System.currentTimeMillis();
+                    totalDuration = stopTime - startTime;
+                    System.out.println("Host acknowledged packet " + clientSent + " after " +
+                            totalDuration + " ms");
+                    received = false;
+                    clientSent++;
+                    
+                }
 		}
                 catch (UnknownHostException ue){
                         System.out.println("Unknown host "+hostname);
@@ -127,7 +159,8 @@ public class UserDatagramProtocol extends Thread{
 		catch (IOException e){
 			System.out.println ("Error - " + e);
 		}
-            }    
+            }
+        }
     }
     /**
      * @param args the command line arguments
